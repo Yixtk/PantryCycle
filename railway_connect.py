028c4c76d0f5557efcc4ad69bdcf45cc3b6c04ed
al8544 <al8544@stern.nyu.edu>
2025-11-20 18:53:58 -0500

    Add files via upload

--- railway_connect.py
+++ railway_connect.py
@@ -8,7 +8,7 @@ def create_connection():
     try:
         connection = psycopg2.connect(
             host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT', '5432'),
+            port=os.getenv('DB_PORT'),
             database=os.getenv('DB_NAME'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD')
